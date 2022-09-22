546a50d33f8d525d189871aed40e06e6c5fac8ff
kaarthiksundar <kaarthik01sundar@gmail.com>
2022-09-22 12:10:14 -0600

    initial model complete

--- src/data_parser.jl
+++ src/data_parser.jl
@@ -8,7 +8,7 @@ function parse_case_data(file)
         cost = gen["cost"][2:2:end]
         best_slope = sum(pg .* cost) / sum(pg.^2)
         gen["model"] = 2
-        gen["cost"] = [best_slope, 0.0] 
+        gen["cost"] = [best_slope/100.0, 0.0] 
         gen["ncost"] = 2
     end 
     
@@ -24,4 +24,14 @@ function get_ref(data)
     ref = PowerModels.build_ref(data)
     PowerModels.ref_add_on_off_va_bounds!(ref, data)
     return ref[:it][:pm][:nw][0]
+end 
+
+function add_costs(ref, ramp_scaling, load_shed_scaling) 
+    for (_, gen) in ref[:gen]
+        gen["ramping_cost"] = ramp_scaling * gen["cost"][1] * 0.01
+    end
+    max_generation_cost = [gen["cost"][1] for (_, gen) in ref[:gen]] |> maximum
+    for (_, load) in ref[:load]
+        load["shedding_cost"] = load_shed_scaling * max_generation_cost * 10.0
+    end 
 end 
--- src/preventive.jl
+++ src/preventive.jl
@@ -1,6 +1,10 @@
-function create_preventive_model(ref; budget::Int = 5, num_scenarios::Int = 10)
+function create_preventive_model(ref; budget::Int = 5, 
+    num_scenarios::Int = 10, 
+    ramping_scaling::Float64 = 1.0, 
+    load_shed_scaling::Float64 = 1.0)
 
     scenarios = generate_scenarios(ref, num_scenarios)
+    add_costs(ref, ramping_scaling, load_shed_scaling)
     var = Dict{Symbol,Any}()
     extra = Dict{Symbol,Any}()
 
@@ -103,13 +107,18 @@ function create_preventive_model(ref; budget::Int = 5, num_scenarios::Int = 10)
             lower_bound = ref[:gen][i]["pmin"], 
             upper_bound = ref[:gen][i]["pmax"]
         )
-        var[:t_g_scenario] = @recourse(sp, 
-            t_g_scenario[i in keys(ref[:gen])], 
+        var[:ramp_g_plus_scenario] = @recourse(sp, 
+            ramp_g_plus_scenario[i in keys(ref[:gen])], 
             lower_bound = 0.0,
             upper_bound = ref[:gen][i]["pmax"]
         )
-        var[:load_shed_scenario] = @recourse(sp, 
-            load_shed_scenario[i in keys(ref[:load])], 
+        var[:ramp_g_minus_scenario] = @recourse(sp, 
+            ramp_g_minus_scenario[i in keys(ref[:gen])], 
+            lower_bound = 0.0,
+            upper_bound = ref[:gen][i]["pmin"]
+        )
+        var[:load_scenario] = @recourse(sp, 
+            load_scenario[i in keys(ref[:load])], 
             lower_bound = 0.0, 
             upper_bound = ref[:load][i]["pd"]    
         )
@@ -122,97 +131,82 @@ function create_preventive_model(ref; budget::Int = 5, num_scenarios::Int = 10)
         p_expr_scenario = merge(p_expr_scenario, Dict([((l,j,i), -1.0 * p_scenario[(l,i,j)]) for (l,i,j) in ref[:arcs_from]]))
         extra[:p_expr_scenario] = p_expr_scenario
         var[:p_dc_scenario] = @recourse(sp, p_dc_scenario[a in ref[:arcs_dc]])
-
-    end 
-    # @stochastic_model m begin 
-    #     @stage 1 begin 
-            
-    #     end 
-
-    #     @stage 2 begin 
-    #         @uncertain ξ[(l,i,j) in ref[:arcs_from]] 
+        for (l, dcline) in ref[:dcline]
+            f_idx = (l, dcline["f_bus"], dcline["t_bus"])
+            t_idx = (l, dcline["t_bus"], dcline["f_bus"])
     
-            
-    #         for (l, dcline) in ref[:dcline]
-    #             f_idx = (l, dcline["f_bus"], dcline["t_bus"])
-    #             t_idx = (l, dcline["t_bus"], dcline["f_bus"])
-        
-    #             JuMP.set_lower_bound(p_dc_scenario[f_idx], dcline["pminf"])
-    #             JuMP.set_upper_bound(p_dc_scenario[f_idx], dcline["pmaxf"])
-        
-    #             JuMP.set_lower_bound(p_dc_scenario[t_idx], dcline["pmint"])
-    #             JuMP.set_upper_bound(p_dc_scenario[t_idx], dcline["pmaxt"])
-    #         end
-
-    #     end 
-    # end 
-
-
-    """
-    # add objective cost;model is always 2 and order is always 1 (data has been fixed to give zero constant)
-    JuMP.@objective(m, Min, sum(gen["cost"][1] * pg[i] for (i, gen) in ref[:gen]))
-
-    # fix the voltage angle to zero at the reference bus
-    for (i, _) in ref[:ref_buses]
-        JuMP.@constraint(m, va[i] == 0)
-    end
-
-    # nodal power balance constraints
-    for (i, _) in ref[:bus]
-        # Build a list of the loads and shunt elements connected to the bus i
-        bus_loads = [ref[:load][l] for l in ref[:bus_loads][i]]
-        bus_shunts = [ref[:shunt][s] for s in ref[:bus_shunts][i]]
-
-        # Active power balance at node i
-        JuMP.@constraint(m,
-            sum(p_expr[a] for a in ref[:bus_arcs][i]) +             # sum of active power flow on lines from bus i +
-            sum(p_dc[a_dc] for a_dc in ref[:bus_arcs_dc][i]) ==     # sum of active power flow on HVDC lines from bus i =
-            sum(pg[g] for g in ref[:bus_gens][i]) -                 # sum of active power generation at bus i -
-            sum(load["pd"] for load in bus_loads) -                 # sum of active load consumption at bus i -
-            sum(shunt["gs"] for shunt in bus_shunts)*1.0^2          # sum of active shunt element injections at bus i
+            JuMP.set_lower_bound(p_dc_scenario[f_idx], dcline["pminf"])
+            JuMP.set_upper_bound(p_dc_scenario[f_idx], dcline["pmaxf"])
+    
+            JuMP.set_lower_bound(p_dc_scenario[t_idx], dcline["pmint"])
+            JuMP.set_upper_bound(p_dc_scenario[t_idx], dcline["pmaxt"])
+        end
+        # add ramping + load shedding costs
+        JuMP.@objective(sp, Min, 
+            sum((gen["cost"][1] + gen["ramping_cost"]) * ramp_g_plus_scenario[i] for (i, gen) in ref[:gen]) + 
+            sum(gen["ramping_cost"] * ramp_g_minus_scenario[i] for (i, gen) in ref[:gen]) + 
+            sum(load["shedding_cost"] * (load["pd"] - load_scenario[i]) for (i, load) in ref[:load])
         )
-    end
 
-    # ohms law on-off version + voltage difference on-off version + thermal limit on-off 
-    for (i, branch) in ref[:branch]
-        f_bus = branch["f_bus"]
-        t_bus = branch["t_bus"]
-        f_idx = (i, f_bus, t_bus)
-
-        _, b = calc_branch_y(branch)
-
-        vad_min = ref[:off_angmin]
-        vad_max = ref[:off_angmax]
-
-        p_fr  = p[f_idx]
-        va_fr = va[f_bus]
-        va_to = va[t_bus]
-        z = z_branch[i]
-
-        if b <= 0
-            JuMP.@constraint(m, p_fr <= -b * (va_fr - va_to + vad_max * (1-z)))
-            JuMP.@constraint(m, p_fr >= -b * (va_fr - va_to + vad_min * (1-z)))
-        else # account for bound reversal when b is positive
-            JuMP.@constraint(m, p_fr >= -b * (va_fr - va_to + vad_max * (1-z)))
-            JuMP.@constraint(m, p_fr <= -b * (va_fr - va_to + vad_min * (1-z)))
+        for (i, _) in ref[:ref_buses]
+            @constraint(sp, va_scenario[i] == 0)
         end
 
-        angmin = branch["angmin"]
-        angmax = branch["angmax"]
+        for (i, _) in ref[:gen]
+            @constraint(sp, ramp_g_scenario[i] == ramp_g_plus_scenario[i] - ramp_g_minus_scenario[i])
+        end     
 
-        JuMP.@constraint(m, va_fr - va_to <= angmax*z + vad_max * (1-z))
-        JuMP.@constraint(m, va_fr - va_to >= angmin*z + vad_min * (1-z))
+        for (i, _) in ref[:bus]
+            # Build a list of the shunt elements connected to the bus i
+            bus_shunts = [ref[:shunt][s] for s in ref[:bus_shunts][i]]
+    
+            # Active power balance at node i
+            @constraint(sp,
+                sum(p_expr_scenario[a] for a in ref[:bus_arcs][i]) +             # sum of active power flow on lines from bus i +
+                sum(p_dc_scenario[a_dc] for a_dc in ref[:bus_arcs_dc][i]) ==     # sum of active power flow on HVDC lines from bus i =
+                sum(pg[g] + ramp_g_scenario[g] for g in ref[:bus_gens][i]) -     # sum of active power generation at bus i -
+                sum(load_scenario[l] for l in ref[:bus_loads][i]) -              # sum of active load consumption at bus i -
+                sum(shunt["gs"] for shunt in bus_shunts)*1.0^2                   # sum of active shunt element injections at bus i
+            )
+        end
 
-        rate_a = branch["rate_a"]
-        
-        JuMP.@constraint(m, p_fr <=  rate_a * z)
-        JuMP.@constraint(m, p_fr >= -rate_a * z)
+        for (i, branch) in ref[:branch]
+            f_bus = branch["f_bus"]
+            t_bus = branch["t_bus"]
+            f_idx = (i, f_bus, t_bus)
+    
+            _, b = calc_branch_y(branch)
+    
+            vad_min = ref[:off_angmin]
+            vad_max = ref[:off_angmax]
+    
+            p_fr  = p_scenario[f_idx]
+            va_fr = va_scenario[f_bus]
+            va_to = va_scenario[t_bus]
+            z = z_branch[i]
+    
+            if b <= 0
+                @constraint(sp, p_fr <= -b * (va_fr - va_to + vad_max * (1 - z * (1 - ξ[f_idx]))))
+                @constraint(sp, p_fr >= -b * (va_fr - va_to + vad_min * (1 - z * (1 - ξ[f_idx]))))
+            else # account for bound reversal when b is positive
+                @constraint(sp, p_fr >= -b * (va_fr - va_to + vad_max * (1 - z * (1 - ξ[f_idx]))))
+                @constraint(sp, p_fr <= -b * (va_fr - va_to + vad_min * (1 - z * (1 - ξ[f_idx]))))
+            end
+    
+            angmin = branch["angmin"]
+            angmax = branch["angmax"]
+    
+            @constraint(sp, va_fr - va_to <= angmax*z + vad_max * (1 - z * (1 - ξ[f_idx])))
+            @constraint(sp, va_fr - va_to >= angmin*z + vad_min * (1 - z * (1 - ξ[f_idx])))
+    
+            rate_a = branch["rate_a"]
+            
+            @constraint(sp, p_fr <=  rate_a * z * (1 - ξ[f_idx]))
+            @constraint(sp, p_fr >= -rate_a * z * (1 - ξ[f_idx]))
+        end 
     end 
 
-    # budget constraints on line switching
-    JuMP.@constraint(m, sum((1 - z_branch[i]) for i in keys(ref[:branch])) <= budget)
-    """
-    # return opt_model
+    return (model = sp, scenarios = scenarios, var = var, extra = extra)
 end 
 
 # function solve_topology_control_model(opt_model::OptModel, optimizer)
