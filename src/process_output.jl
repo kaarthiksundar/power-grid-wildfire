f465285ac5973bc9eff25492160cd3efa9e5f432
kaarthiksundar <kaarthik01sundar@gmail.com>
2022-10-11 15:43:04 -0600

    adds better solution processsing

--- src/process_output.jl
+++ src/process_output.jl
@@ -1,7 +1,7 @@
 using JSON 
 using DelimitedFiles
 
-function get_result_file_with_path(load_factor::Float64, parallel::Bool)
+function get_result_file_with_path(parallel::Bool)
     file_names = []
     preventive_path = "./output/preventive/"
     corrective_path = "./output/corrective/"
@@ -9,26 +9,27 @@ function get_result_file_with_path(load_factor::Float64, parallel::Bool)
     for i in 20:20:200
         num_scenarios = string(i)
         file = "budget_5_load_factor_" * 
-            string(Int(load_factor * 100.0)) * serial_parallel * 
+            string(Int(1.00 * 100.0)) * serial_parallel * 
             "_num_scenarios_" * num_scenarios * "_notopology.json"
-        push!(file_names, (preventive_path * file, corrective_path * file))
+        push!(file_names, (1.00, preventive_path * file, corrective_path * file))
     end 
     file = "budget_5_load_factor_" * 
             string(Int(1.05 * 100.0)) * serial_parallel * 
             "_num_scenarios_20_notopology.json"
-    push!(file_names, (preventive_path * file, corrective_path * file))
+    push!(file_names, (1.05, preventive_path * file, corrective_path * file))
     return file_names
 end 
 
 function get_solve_stats(files)
     results = []
-    for (preventive, corrective) in files 
+    for (lf, preventive, corrective) in files 
         preventive_data = JSON.parsefile(preventive)
         corrective_data = JSON.parsefile(corrective)
         preventive_obj = preventive_data["objective_value"]
         corrective_obj = corrective_data["objective_value"]
         rel_gap = (preventive_obj - corrective_obj)/corrective_obj * 100.0
         row = Vector{Union{Float64,Int}}()
+        push!(row, trunc(lf, digits=2))
         push!(row, preventive_data["num_scenarios"] |> Int)
         push!(row, preventive_data["objective_value"])
         push!(row, preventive_data["num_iterations"] |> Int)
@@ -48,10 +49,10 @@ function write_csv(file, header, rows)
     end
 end 
 
-function process_solution(;load_factor::Float64 = 1.00, parallel::Bool=false)
-    files = get_result_file_with_path(load_factor, parallel)
+function process_solution(;parallel::Bool=false)
+    files = get_result_file_with_path(parallel)
     rows = get_solve_stats(files)
-    header = ["# scenarios", "preventive obj. val.", "preventive iter.", "preventive time",
+    header = ["load factor", "# scenarios", "preventive obj. val.", "preventive iter.", "preventive time",
         "corrective obj. val.", "corrective iter.", "corrective time", "obj. rel. gap"]
-    write_csv("./output/solution_stats_" * string(Int(load_factor * 100.0)) * ".csv", header, rows)
+    write_csv("./output/solution_stats.csv", header, rows)
 end 
