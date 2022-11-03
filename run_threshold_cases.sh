fdcd0967ec0acadfd141eb331ba36ad1bfb42531
kaarthiksundar <kaarthik01sundar@gmail.com>
2022-11-03 14:19:35 -0600

    more threshold runs

--- run_threshold_cases.sh
+++ run_threshold_cases.sh
@@ -1,3 +1,15 @@
+julia --project=. src/main.jl --model corrective --switch_budget 5 --load_weighting_factor 1.00 --result_folder ./output/ -s 50
+julia --project=. src/main.jl --model corrective --switch_budget 5 --load_weighting_factor 1.05 --result_folder ./output/ -s 50
+julia --project=. src/main.jl --model preventive --switch_budget 5 --load_weighting_factor 1.00 --result_folder ./output/ -s 50
+julia --project=. src/main.jl --model preventive --switch_budget 5 --load_weighting_factor 1.05 --result_folder ./output/ -s 50
+julia --project=. src/main.jl --model corrective --switch_budget 5 --load_weighting_factor 1.00 --result_folder ./output/ -s 100
+julia --project=. src/main.jl --model corrective --switch_budget 5 --load_weighting_factor 1.05 --result_folder ./output/ -s 100
+julia --project=. src/main.jl --model preventive --switch_budget 5 --load_weighting_factor 1.00 --result_folder ./output/ -s 100
+julia --project=. src/main.jl --model preventive --switch_budget 5 --load_weighting_factor 1.05 --result_folder ./output/ -s 100
+julia --project=. src/main.jl --model corrective --switch_budget 5 --load_weighting_factor 1.00 --result_folder ./output/ -s 150
+julia --project=. src/main.jl --model corrective --switch_budget 5 --load_weighting_factor 1.05 --result_folder ./output/ -s 150
+julia --project=. src/main.jl --model preventive --switch_budget 5 --load_weighting_factor 1.00 --result_folder ./output/ -s 150
+julia --project=. src/main.jl --model preventive --switch_budget 5 --load_weighting_factor 1.05 --result_folder ./output/ -s 150
 julia --project=. src/main.jl --model corrective --switch_budget 5 --load_weighting_factor 1.00 --result_folder ./output/ -s 200
 julia --project=. src/main.jl --model corrective --switch_budget 5 --load_weighting_factor 1.05 --result_folder ./output/ -s 200
 julia --project=. src/main.jl --model preventive --switch_budget 5 --load_weighting_factor 1.00 --result_folder ./output/ -s 200
