95369e630f6e03290d670255352a71b7de87bf78
kaarthiksundar <kaarthik01sundar@gmail.com>
2022-11-15 09:44:56 -0700

    updates no-threshold results

--- src/process_output.jl
+++ src/process_output.jl
@@ -3,8 +3,8 @@ using DelimitedFiles
 
 function get_result_file_with_path()
     file_names = []
-    preventive_path = "./output/preventive/"
-    corrective_path = "./output/corrective/"
+    preventive_path = "./output/no-threshold/preventive/"
+    corrective_path = "./output/no-threshold/corrective/"
     for i in 20:20:200
         num_scenarios = string(i)
         serial_file = "budget_5_load_factor_" * 
@@ -36,47 +36,66 @@ function get_result_file_with_path()
     return file_names
 end 
 
-function get_solve_stats(files)
+function get_solve_stats(files; parallel::Bool = false)
     results = []
     for (lf, s_p, s_c, p_p, p_c) in files 
         (!isfile(s_p) || !isfile(s_c) || !isfile(p_p) || !isfile(p_c)) && (continue)
         s_p_data = JSON.parsefile(s_p)
         s_c_data = JSON.parsefile(s_c)
-        p_p_data = JSON.parsefile(p_p)
-        p_c_data = JSON.parsefile(p_c)
+        if parallel
+            p_p_data = JSON.parsefile(p_p)
+            p_c_data = JSON.parsefile(p_c)
+        end 
 
         s_p_obj = s_p_data["objective_value"]
         s_c_obj = s_c_data["objective_value"]
-        p_p_obj = p_p_data["objective_value"]
-        p_c_obj = p_c_data["objective_value"]
         s_rel_gap = trunc((s_p_obj - s_c_obj)/s_c_obj * 100.0; digits=2)
-        p_rel_gap = trunc((p_p_obj - p_c_obj)/p_c_obj * 100.0; digits=2)
-        prev_rel_gap = trunc(abs(s_p_obj - p_p_obj)/s_p_obj * 100.0; digits=2)
-        corr_rel_gap = trunc(abs(s_c_obj - p_c_obj)/s_c_obj * 100.0; digits=2)
+        if parallel 
+            p_p_obj = p_p_data["objective_value"]
+            p_c_obj = p_c_data["objective_value"]
+            p_rel_gap = trunc((p_p_obj - p_c_obj)/p_c_obj * 100.0; digits=2)
+        end 
+        
+        if parallel
+            prev_rel_gap = trunc(abs(s_p_obj - p_p_obj)/s_p_obj * 100.0; digits=2)
+            corr_rel_gap = trunc(abs(s_c_obj - p_c_obj)/s_c_obj * 100.0; digits=2)
+        end
 
         s_p_time = s_p_data["solve_time"]
         s_c_time = s_c_data["solve_time"]
-        p_p_time = p_p_data["solve_time"]
-        p_c_time = p_c_data["solve_time"]
+        if parallel
+            p_p_time = p_p_data["solve_time"]
+            p_c_time = p_c_data["solve_time"]
+        end 
 
         s_p_iter = s_p_data["num_iterations"] |> Int
         s_c_iter = s_c_data["num_iterations"] |> Int
-        p_p_iter = p_p_data["num_iterations"] |> Int
-        p_c_iter = p_c_data["num_iterations"] |> Int
+        if parallel
+            p_p_iter = p_p_data["num_iterations"] |> Int
+            p_c_iter = p_c_data["num_iterations"] |> Int
+        end 
 
         row = Vector{Union{Float64,Int}}()
         
         push!(row, trunc(lf, digits=2))
         push!(row, s_p_data["num_scenarios"] |> Int)
         
-        append!(row, [s_p_time, p_p_time, s_c_time, p_c_time])
-        
-        append!(row, [s_p_obj, s_c_obj, s_rel_gap])
-        append!(row, [p_p_obj, p_c_obj, p_rel_gap])
-        append!(row, [prev_rel_gap, corr_rel_gap])
+        if parallel 
+            append!(row, [s_p_time, p_p_time, s_c_time, p_c_time])
+        else 
+            append!(row, [s_p_time, s_c_time])
+        end
 
-        append!(row, [s_p_iter, p_p_iter])
-        append!(row, [s_c_iter, p_c_iter])
+        append!(row, [s_p_obj, s_c_obj, s_rel_gap])
+        (parallel) && (append!(row, [p_p_obj, p_c_obj, p_rel_gap]))
+        (parallel) && (append!(row, [prev_rel_gap, corr_rel_gap]))
+
+        if parallel 
+            append!(row, [s_p_iter, p_p_iter])
+            append!(row, [s_c_iter, p_c_iter])
+        else 
+            append!(row, [s_p_iter, s_c_iter])
+        end 
 
         push!(results, row)
     end 
@@ -89,16 +108,25 @@ function write_csv(file, header, rows)
     end
 end 
 
-function process_solution()
+function process_solution(; parallel::Bool = false)
     files = get_result_file_with_path()
-    rows = get_solve_stats(files)
-    header = ["load factor", "# scenarios", 
+    rows = get_solve_stats(files; parallel = parallel)
+    if (parallel)
+        header = ["load factor", "# scenarios", 
         "serial prev. time", "parallel prev. time", 
         "serial corr. time", "parallel corr. time", 
         "serial prev. obj. val.", "serial corr. obj. val.", "serial obj. rel. gap.", 
         "parallel prev. obj. val.", "parallel corr. obj. val.", "parallel obj. rel. gap.", 
         "prev. ser. to par. rel. gap", "corr. ser. to par. rel. gap", 
         "serial prev. iter.", "parallel prev. iter.", 
-        "serial corr. iter.", "parallel corr. iter.",]
-    write_csv("./output/solution_stats.csv", header, rows)
+        "serial corr. iter.", "parallel corr. iter."]
+    else 
+        header = ["load factor", "# scenarios", 
+        "serial prev. time", 
+        "serial corr. time", 
+        "serial prev. obj. val.", "serial corr. obj. val.", "serial obj. rel. gap.", 
+        "serial prev. iter.",
+        "serial corr. iter."]
+    end
+    write_csv("./output/solution_stats_no_threshold.csv", header, rows)
 end 
